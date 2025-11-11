b8913e5b6b1da66310bf4ca90819228070f81705
zsolnaih <zsolnaih@gmail.com>
2025-11-11 15:00:55 +0100

    k8s install with kubeadm

--- Terraform/Nginx/envs/default/main.tf
+++ Terraform/Nginx/envs/default/main.tf
@@ -8,19 +8,19 @@ module "vpc" {
     single_nat = var.single_nat  
 }
 
-module "web" {
-    source = "../../modules/web"
-    private_subnets = module.vpc.private_subnets_ids
-    public_subnets = module.vpc.public_subnets_ids
-    project_name = var.project_name
-    vpc_id = module.vpc.vpc_id
-    desired_capacity = var.desired_capacity
-    min_size = var.min_size
-    max_size = var.max_size
-    instance_type = var.instance_type
-    ssm_managed = var.ssm_managed
-    allowed_http_cidrs = var.allowed_http_cidrs
+# module "web" {
+#     source = "../../modules/web"
+#     private_subnets = module.vpc.private_subnets_ids
+#     public_subnets = module.vpc.public_subnets_ids
+#     project_name = var.project_name
+#     vpc_id = module.vpc.vpc_id
+#     desired_capacity = var.desired_capacity
+#     min_size = var.min_size
+#     max_size = var.max_size
+#     instance_type = var.instance_type
+#     ssm_managed = var.ssm_managed
+#     allowed_http_cidrs = var.allowed_http_cidrs
 
-    depends_on = [ module.vpc ]
-}
+#     depends_on = [ module.vpc ]
+# }
 
--- Terraform/Nginx/envs/default/terraform.tf
+++ Terraform/Nginx/envs/default/terraform.tf
@@ -1,11 +1,11 @@
 terraform {
-  backend "s3" {
-    bucket       = "henrik-terraform-backend"
-    key          = "nginx/tfstate/default/terraform.tfstate"
-    region       = "eu-central-1"
-    use_lockfile = true
+  # backend "s3" {
+  #   bucket       = "henrik-terraform-backend"
+  #   key          = "nginx/tfstate/default/terraform.tfstate"
+  #   region       = "eu-central-1"
+  #   use_lockfile = true
 
-  }
+  # }
   required_version = ">= 1.6.0"
   required_providers {
     aws = {
